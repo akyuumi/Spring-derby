507f8c2773e5c2e696e0f18e5c3f54fc656c7cde
akyuumi <akyuumi.pg@gmail.com>
2023-11-25 20:52:43 +0900

    テストクラス追加

---
+++ src/test/java/spring/derby/MyApplicationRunnerTest.java
@@ -0,0 +1,14 @@
+package spring.derby;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyApplicationRunnerTest {
+
+	@Test
+	void test() {
+		MyApplicationRunner runner = new MyApplicationRunner();
+	}
+
+}
